c077f4115997b4873eca0866ac37579c8515a2f7
Gabriel Araujo <gabrielaraujovni@gmail.com>
2024-09-05 17:28:04 -0300

    Renderizacao condicional

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,9 @@
-import { Person } from "@/components/Person"
 import { Card } from "@/components/Card"
 
 const Page = () => {
   return (
     <div>
-      <h1 className="font-bold">Olá, Mundo!</h1>
-      {/* <GeoForms/> */}
-
-      <Person
-        name="Gabriel de Araujo"
-        avatar="https://media.licdn.com/dms/image/v2/C4D03AQGNs-ODK7wQSw/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1652511178467?e=1730937600&v=beta&t=VD759uRgVEd5vxPtjBFEoeZ0SlMATOJeLBjq-W6Peq4"
-        roles={["Teste 1", "Teste 2"]}
-      />
-
-      <Person
-        name="Ana Valéria"
-        avatar="https://media.licdn.com/dms/image/v2/D4D03AQFuXRqiTCFeBg/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1718254169446?e=1730937600&v=beta&t=sgtFs6AHP-MlbE7nzQSVijbNo_N5WV-nq4k2e8GgsO0"
-        roles={["Teste 1", "Teste 2"]}
-      />
-
-      <Person
-        name="Cadu"
-        avatar="./cadu.jpg"
-        roles={["Teste 1", "Teste 2"]}
-      />
-      <Person
-        name="Fulano"
-        roles={["Teste 1", "Teste 2"]}
-      />
-      <Card>Frase de efeito do Card</Card>
-
+      <Card phrase="A vingança nunca é plena, mata a alma e a envenena." />
     </div>
   )
 }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,10 +1,19 @@
+import { ReactNode } from "react"
+
 type Props = {
-    children: string
+    phrase: string
+    author?: string
 }
-export const Card = ({children}: Props) => {
+export const Card = ({ phrase, author }: Props) => {
     return (
-        <div className="border-2 border-red-400 p-3 text-3xl text-center">
-            {children}
+        <div className="italic w-96 border-2 border-red-400 p-3 text-3xl text-center">
+            <h3 className="p-3 text-3xl text-center font-bold">"{phrase}"</h3>
+            <p className="p-3 text-sm text-right">- {author ?? "Autor desconhecido"}</p>
+            {/* 
+            <p className="p-3 text-sm text-right">- {author ? author: "Author desconhecido"}</p> 
+                Dois metodos de como usar condicionais ternarias
+            */}
         </div>
     )
+
 }
